e6e62e16d1d29a6b8d6ddc308d4aa9f0b3d2d947
leey0818 <leey0818@gmail.com>
2019-06-18 22:54:27 +0900

    add: spring-cloud-config-server

--- helloworld-service/src/main/java/com/example/demo/controller/ServiceController.java
+++ helloworld-service/src/main/java/com/example/demo/controller/ServiceController.java
@@ -1,20 +1,25 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@RefreshScope
 @RestController
 public class ServiceController {
 
     @Value("${eureka.instance.metadataMap.instanceId}")
     String instanceId;
     
+    @Value("${example.hello}")
+    String message;
+    
     @RequestMapping(value = "/", method = RequestMethod.GET)
     String message() {
-        return "hello!";
+        return this.message;
     }
     
     @RequestMapping("/id")
